154431c5d8337a531fd45b9b6f16da7a4d50d4cf
Your Name <you@example.com>
2025-07-30 20:43:26 +0000

    new function added

---
+++ dines_math.c
@@ -0,0 +1,16 @@
+int sum(int a, int b) {
+    return a + b;
+}
+int product(int a, int b) {
+    return a * b;
+}
+int sub(int a, int b) {
+    return a - b;
+}
+int div(int a, int b) {
+    return a / b;
+}
+int mod(int a, int b) {
+    return a%b;
+}
+
